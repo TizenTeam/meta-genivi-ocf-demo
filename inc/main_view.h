a41bf4f85ffd04530ab58c5b2083743ceb2bb0e5
Philippe Coval <philippe.coval@osg.samsung.com>
2016-04-18 10:56:24 +0200

    WIP: mod: inc/main_view.h (sandbox/pcoval/wip/maps/tizen/wearable-2.3.1)
    Change-Id: I5a3a71a79f9097d154cb38da7a3911a11ba9ac27
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- inc/main_view.h
+++ inc/main_view.h
@@ -3,11 +3,7 @@
 
 #include <Evas.h>
 #include <Elementary.h>
-#include <maps_service.h>
 
-#include "revgeocode.h"
-#include "place.h"
-#include "route.h"
 
 #define MAP_VIEW_EDJ_FILE "edje/mainview.edj"
 
@@ -18,18 +14,12 @@ typedef enum
 	MAPS_VIEW_MODE_DIRECTION
 } MAP_VIEW_MODE;
 
-void create_maps_service_handle();
-maps_service_h get_maps_service_handle();
-void destroy_maps_service_handle();
 
 Evas_Object* create_map_view(Evas_Object *parent);
 
 void map_get_poi_lat_lng(double *latitude, double *longitude);
 void map_get_selected_poi_lat_lng(double *latitude, double *longitude, char **name);
 
-void handle_addr_notification(revgeocode_s *result);
-void handle_poi_notification(place_s **place_res, int res_cnt);
-void handle_route_notification(route_s *result);
 
 void hide_map_poi_overlays(Eina_Bool enable);
 void remove_map_poi_overlays();
